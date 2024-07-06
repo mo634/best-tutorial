74c439c3988b1c3815e09afeac79a674e74f9910
MohamedMostafa <83365873+mo634@users.noreply.github.com>
2024-07-06 11:46:17 +0300

    create service

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -2,8 +2,10 @@
 
 import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
+import { TasksService } from './tasks.service';
 
 @Module({
   controllers: [TasksController],
+  providers: [TasksService],
 })
 export class TasksModule {}

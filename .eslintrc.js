3ce6938cb2dc2745c1b385b19cc39760dab0a968
MohamedMostafa <83365873+mo634@users.noreply.github.com>
2024-07-06 12:09:32 +0300

    example for service functionallity and inject to controller to be ready for incomming request

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,8 +24,13 @@ module.exports = {
     'prettier/prettier': [
       'error',
       {
-        'endOfLine': 'auto',
+        endOfLine: 'auto',
       },
+      "error",
+      {
+        "singleQuote": true,
+        "parser": "flow"
+      }
     ],
   },
 };
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,5 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class TasksService {
-    
+    private tasks = ["mohamed" , "ahmed","manar"];
+    getAllTasks() {
+        return this.tasks;
+    }
 }
